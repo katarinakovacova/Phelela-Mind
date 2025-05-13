13f56a0f737d64e0a74eba1e4800a4aca9061d6b
katarinakovacova <katarina.sorens@gmail.com>
2025-05-13 09:34:15 +0200

    Implement Calendar Screen #12

--- app/src/main/java/com/example/phelela_mind/ui/screens/CalendarScreen.kt
+++ app/src/main/java/com/example/phelela_mind/ui/screens/CalendarScreen.kt
@@ -1,32 +1,60 @@
 package com.example.phelela_mind.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.util.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalendarScreen(innerPadding: PaddingValues) {
+    val datePickerState = rememberDatePickerState()
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier
             .padding(innerPadding)
-            .fillMaxSize().
-            background(Color.White),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Calendar Screen",
-            fontSize = 40.sp,
-            color = Color.Black
+            text = "Select a date",
+            fontSize = 30.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 24.dp, bottom = 16.dp)
+        )
+
+        DatePicker(
+            state = datePickerState,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        val selectedDateMillis = datePickerState.selectedDateMillis
+        val formattedDate = remember(selectedDateMillis) {
+            selectedDateMillis?.let {
+                SimpleDateFormat("d MMMM yyyy", Locale("en", "GB")).format(Date(it))
+            } ?: "No date selected"
+        }
+
+        Text(
+            text = "Selected date: $formattedDate",
+            fontSize = 18.sp,
+            color = Color.DarkGray,
+            modifier = Modifier.padding(top = 16.dp)
         )
     }
 }
+
+
